b7d252ee8eb39e3788b33dee656bdae703297f70
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 17:56:21 -0600

    Update Command.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/commands/Command.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/commands/Command.java
@@ -1,1 +1,9 @@
+package edu.du.ict_4315.commands;
 
+import java.util.Properties;
+
+public interface Command {
+    String getCommandName();
+    String getDisplayName();
+    String execute(Properties params);
+}
