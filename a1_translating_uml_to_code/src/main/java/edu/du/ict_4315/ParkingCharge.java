06cbc46b76822b9a7c2cb578df51e094620e4506
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:02:07 -0600

    Update ParkingCharge.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingCharge.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingCharge.java
@@ -1,1 +1,29 @@
+package edu.du.ict_4315
 
+import java.time.Instant;
+
+public class ParkingCharge {
+ private String permitId;
+ private String lotId;
+ private Instant incurred;
+ Money amount;
+
+ public ParkingCharge(String permitId, String lotId, Instant incurred, Money amount) {
+     this.permitId = permitId;
+     this.lotId = lotId;
+     this.incurred = incurred;
+     this.amount = amount;
+ }
+
+
+
+public ParkingCharge(Money chargeAmount, Customer testCustomer) {
+
+}
+
+
+
+public String toString() {
+     return "Charge[Permit: " + permitId + ", Lot: " + lotId + ", Time: " + incurred + ", Amount: " + amount + "]";
+ }
+}
