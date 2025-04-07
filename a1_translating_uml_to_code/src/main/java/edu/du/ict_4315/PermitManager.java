adc59e24667db6e42fe3122c275d99e94a206ba6
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:04:30 -0600

    Update PermitManager.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/PermitManager.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/PermitManager.java
@@ -1,1 +1,40 @@
+package edu.du.ict_4315
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PermitManager {
+    private HashMap<String, ParkingPermit> permits;
+
+    public PermitManager() {
+        this.permits = new HashMap<>();
+    }
+
+
+    public ParkingPermit register(Car car) {
+        String permitId = "PERMIT-" + car.getLicense();
+        if (permits.containsKey(permitId)) {
+            return permits.get(permitId);  
+        }
+
+        ParkingPermit permit = new ParkingPermit(permitId, car);
+        permits.put(permitId, permit);
+        return permit;
+    }
+
+    public List<String> getAllPermitIds() {
+        return new ArrayList<>(permits.keySet());
+    }
+
+
+    public List<String> getPermitIdsByCustomer(Customer customer) {
+        List<String> permitIds = new ArrayList<>();
+        for (ParkingPermit permit : permits.values()) {
+            if (permit.getCar().getOwner().equals(customer)) {
+                permitIds.add(permit.getId());
+            }
+        }
+        return permitIds;
+    }
+}
