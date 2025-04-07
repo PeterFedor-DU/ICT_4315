527b7defd5bb1b521cfb6bf24935b65b6b84f61c
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:07:32 -0600

    Update CustomerTest.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/CustomerTest.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/CustomerTest.java
@@ -1,1 +1,13 @@
+package edu.du.ict_4315
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+    @Test
+    public void testRegisterCar() {
+        Customer customer = new Customer("CUST1", "Peter Fedor", new Address("123 Wadsworth Blvd", "Westminster", "CO", "80021"), "303-333-3333");
+        Car car = customer.register("1234XYZ", CarType.COMPACT);
+        assertNotNull(car);
+    }
+}
