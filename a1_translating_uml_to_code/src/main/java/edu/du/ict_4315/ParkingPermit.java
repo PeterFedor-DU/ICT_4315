7cffa5cdae857cc9e6a8369117d63e56a87e3ba9
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:03:37 -0600

    Update ParkingPermit.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingPermit.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingPermit.java
@@ -1,1 +1,34 @@
+package edu.du.ict_4315
 
+import java.util.Calendar;
+
+public class ParkingPermit {
+    private String id;
+    private Car car;
+    private Calendar expirationDate;
+    private Calendar registrationDate;
+
+    public ParkingPermit(String id, Car car) {
+        this.id = id;
+        this.car = car;
+        this.registrationDate = Calendar.getInstance();
+        this.expirationDate = (Calendar) registrationDate.clone();
+        this.expirationDate.add(Calendar.YEAR, 1); 
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public Calendar getExpirationDate() {
+        return expirationDate;
+    }
+
+    public Calendar getRegistrationDate() {
+        return registrationDate;
+    }
+}
