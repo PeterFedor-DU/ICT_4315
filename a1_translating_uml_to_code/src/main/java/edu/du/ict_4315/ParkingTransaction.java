a98d2e90e623fa89f98927bed7c54d4bb4834e42
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:04:14 -0600

    Update ParkingTransaction.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingTransaction.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingTransaction.java
@@ -1,1 +1,38 @@
+package edu.du.ict_4315
 
+import java.util.Calendar;
+
+public class ParkingTransaction {
+    private Calendar transactionDate;
+    private ParkingPermit permit;
+    private Money feeCharged;
+
+
+    public ParkingTransaction(Calendar transactionDate, ParkingPermit permit, ParkingLot lot) {
+        this.transactionDate = transactionDate;
+        this.permit = permit;
+        this.feeCharged = new Money(7.50);
+    }
+
+
+    public Calendar getTransactionDate() {
+        return transactionDate;
+    }
+
+///checking if the transaction happened today
+    public boolean isTransactionToday() {
+        Calendar today = Calendar.getInstance();
+        return today.get(Calendar.YEAR) == transactionDate.get(Calendar.YEAR) &&
+               today.get(Calendar.MONTH) == transactionDate.get(Calendar.MONTH) &&
+               today.get(Calendar.DAY_OF_MONTH) == transactionDate.get(Calendar.DAY_OF_MONTH);
+    }
+
+
+    public ParkingPermit getPermit() {
+        return permit;
+    }
+
+    public Money getFeeCharged() {
+        return feeCharged;
+    }
+}  
