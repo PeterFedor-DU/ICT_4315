f76b03aa296d9d7cb2cf421ca7d2d516dfacb747
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 19:01:41 -0600

    Update Money.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/Money.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/Money.java
@@ -1,1 +1,23 @@
+package edu.du.ict_4315
 
+public class Money {
+    private long cents;
+
+    public Money(double d) {
+        this.cents = (long) (d * 100); 
+    }
+
+    public double getDollars() {
+        return cents / 100.0;
+    }
+
+    public Money add(Money other) {
+        long totalCents = this.cents + other.cents;
+        return new Money(totalCents / 100.0);  
+    }
+
+    @Override
+    public String toString() {
+        return "$" + getDollars();
+    }
+}
