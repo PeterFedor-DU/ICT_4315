0c8193e96d19787c3050c4f0b4760b38451c8a3e
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 18:55:55 -0600

    Update Address.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/Address.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/Address.java
@@ -1,1 +1,21 @@
+package edu.du.ict_4315
 
+///Creating a class for the customer address
+public class Address {
+	  private String streetAddress1;
+	  private String streetAddress2;
+	  private String city;
+	  private String state;
+	  private String zipCode;
+
+	  public Address(String streetAddress1, String city, String state, String zipCode) {
+	      this.streetAddress1 = streetAddress1;
+	      this.city = city;
+	      this.state = state;
+	      this.zipCode = zipCode;
+	  }
+
+	  public String getAddressInfo() {
+	      return streetAddress1 + " " + (streetAddress2 != null ? streetAddress2 + ", " : "") + city + ", " + state + " " + zipCode;
+	  }
+	}
