4b1b912d64c399ed894927257c298f5739cfe7c2
PeterFedor-DU <peter.fedor@du.edu>
2025-04-06 18:12:37 -0600

    ParkingService.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/commands/ParkingService.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/commands/ParkingService.java
@@ -1,29 +1,36 @@
+package edu.du.ict_4315.commands;
+
+import edu.du.ict_4315.ParkingOffice
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+///Creating Parking Service class
 public class ParkingService {
     private ParkingOffice office;
-    private Map<String, Command> commands = new HashMap<>();
+    private Map<String, Command> commands;
 
     public ParkingService(ParkingOffice office) {
         this.office = office;
+        this.commands = new HashMap<>();
     }
 
-    public void register(Command cmd) {
-        commands.put(cmd.getCommandName(), cmd);
+    public void register(Command command) {
+        commands.put(command.getCommandName(), command);
     }
 
-    public String performCommand(String name, String[] args) {
-        Command cmd = commands.get(name);
-        if (cmd == null) return "Not a valid input";
+    public String performCommand(String commandName, String[] parameters) {
+        Command command = commands.get(commandName);
+        if (command == null) return "Unknown command: " + commandName;
 
         Properties props = new Properties();
-        for (String arg : args) {
-            String[] keyValue = arg.split("=");
-            props.setProperty(keyValue[0], keyValue[1]);
+        for (String param : parameters) {
+            String[] keyValue = param.split("=", 2);
+            if (keyValue.length == 2) {
+                props.setProperty(keyValue[0], keyValue[1]);
+            }
         }
 
-        return cmd.execute(props);
+        return command.execute(props);
     }
 }
