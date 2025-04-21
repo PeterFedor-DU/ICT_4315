da118d99dbd7b4fe4e5e8ce8a6b824827bf1c13e
PeterFedor-DU <peter.fedor@du.edu>
2025-04-20 21:19:05 -0600

    Update ParkingLot.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingLot.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingLot.java
@@ -8,13 +8,17 @@ public class ParkingLot {
     private String lotId;
     private Address address;
     private int capacity;
-    private List<Car> parkedCars;  
+    private List<Car> parkedCars; 
+    private ParkingCharge pricingStrat;
+    private Money baseRate;
 
     public ParkingLot(String lotId, Address address, int capacity) {
         this.lotId = lotId;
         this.address = address;
         this.capacity = capacity;
         this.parkedCars = new ArrayList<>();
+	this.pricingStrat = pricingStrat;
+        this.baseRate = baseRate;
     }
 
 
@@ -37,10 +41,26 @@ public class ParkingLot {
             }
         }
         
-        public String toString() {
-            return "ParkingLot [ID=" + lotId + ", Address=" + address.getAddressInfo() + ", Capacity=" + capacity + "]";
-        }
 
 
+ public ParkingChargeStrategy getPricingStrat() {
+        return pricingStrat;
+    }
+
+    public void setPricingStrategy(ParkingChargeStrategy pricingStrat) {
+        this.pricingStrat = pricingStrat;
+    }
+
+    public Money getBaseRate() {
+        return baseRate;
+    }
+
+    public void setBaseRate(Money baseRate) {
+        this.baseRate = baseRate;
+    }
+
+    public String toString() {
+        return "ParkingLot [ID=" + lotId + ", Address=" + address.getAddressInfo() + ", Capacity=" + capacity + "]";
+    }
 	
 }
