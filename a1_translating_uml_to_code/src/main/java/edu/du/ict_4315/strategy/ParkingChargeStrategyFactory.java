08326df0057dc40ec7b9156605a1a63bdbed62a4
PeterFedor-DU <peter.fedor@du.edu>
2025-04-27 20:10:44 -0600

    Update ParkingChargeStrategyFactory.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/strategy/ParkingChargeStrategyFactory.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/strategy/ParkingChargeStrategyFactory.java
@@ -1,14 +1,12 @@
-package edu.du.ict_4315.strategy;
+package edu.du.ict_4315;
 
 public class ParkingChargeStrategyFactory {
 
     public static ParkingChargeStrategy getStrategy(ParkingLot lot) {
-///Seeing if the lot has a strategy set
-        if (lot.getPricingStrat() != null) {
-            return lot.getPricingStrat();
-        }
-
-///Setting hourly charge as the default if nothing is returned
-        return new HourlyCharge();
+        if (lot.getPricingStrat() instanceof HourlyCharge) {
+            return new HourlyCharge();
+        } else if (lot.getPricingStrat() instanceof DailyCharge) {
+            return new DailyCharge();
+        } 
     }
 }
