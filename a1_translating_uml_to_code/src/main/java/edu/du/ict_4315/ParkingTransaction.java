903af2cdcfc33d5340d19dcf9d53f7b46ccf2dba
PeterFedor-DU <peter.fedor@du.edu>
2025-04-27 19:57:29 -0600

    Update ParkingTransaction.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingTransaction.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingTransaction.java
@@ -1,32 +1,39 @@
-package edu.du.ict_4315
+package edu.du.ict_4315;
 
 import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class ParkingTransaction {
     private Calendar transactionDate;
     private ParkingPermit permit;
     private Money feeCharged;
 
-
     public ParkingTransaction(Calendar transactionDate, ParkingPermit permit, ParkingLot lot) {
         this.transactionDate = transactionDate;
         this.permit = permit;
-        this.feeCharged = new Money(7.50);
-    }
 
+
+        LocalDateTime entryTime = transactionDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+///setting an exit time for the transaction calc 
+        LocalDateTime exitTime = entryTime.plusHours(1);
+
+///Getting the fee from the charger factory
+        ParkingChargeStrategy strategy = ParkingChargeStrategyFactory.getStrategy(lot);
+        this.feeCharged = strategy.calculateCharge(permit, entryTime, exitTime, lot.getBaseRate());
+    }
 
     public Calendar getTransactionDate() {
         return transactionDate;
     }
 
-///checking if the transaction happened today
+///Seeing if a transaction happened today
     public boolean isTransactionToday() {
         Calendar today = Calendar.getInstance();
         return today.get(Calendar.YEAR) == transactionDate.get(Calendar.YEAR) &&
                today.get(Calendar.MONTH) == transactionDate.get(Calendar.MONTH) &&
                today.get(Calendar.DAY_OF_MONTH) == transactionDate.get(Calendar.DAY_OF_MONTH);
     }
-
 
     public ParkingPermit getPermit() {
         return permit;
@@ -35,4 +42,4 @@ public class ParkingTransaction {
     public Money getFeeCharged() {
         return feeCharged;
     }
-}  
+}
