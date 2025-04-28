5e81c5fc272ebca64c61335a18c1a1f89653381b
PeterFedor-DU <peter.fedor@du.edu>
2025-04-27 20:00:55 -0600

    Update TransactionManager.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/TransactionManager.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/TransactionManager.java
@@ -1,8 +1,10 @@
-package edu.du.ict_4315
+package edu.du.ict_4315;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class TransactionManager {
     private List<ParkingTransaction> transactions;
@@ -11,15 +13,20 @@ public class TransactionManager {
         this.transactions = new ArrayList<>();
     }
 
-
     public ParkingTransaction park(Calendar date, ParkingPermit permit, ParkingLot lot) {
-        ParkingTransaction transaction = new ParkingTransaction(date, permit, lot);
+        LocalDateTime entryTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime exitTime = entryTime.plusHours(1);
+
+        ParkingChargeStrategy strategy = ParkingChargeStrategyFactory.getStrategy(lot);
+        Money feeCharged = strategy.calculateCharge(permit, entryTime, exitTime, lot.getBaseRate());
+///Creating the transaction and storing it
+        ParkingTransaction transaction = new ParkingTransaction(date, permitID, lot, feeCharged);
         transactions.add(transaction);
         return transaction;
     }
 
     public Money getParkingCharges(ParkingPermit permit) {
-        Money totalCharges = new Money(0); 
+        Money totalCharges = new Money(0);
         for (ParkingTransaction transaction : transactions) {
             if (transaction.getPermit().equals(permit)) {
                 totalCharges = totalCharges.add(transaction.getFeeCharged());
@@ -30,12 +37,10 @@ public class TransactionManager {
 
     public Money getParkingCharges(Customer customer) {
         Money totalCharges = new Money(0);  
-      
         for (Car car : customer.getCars()) {
             String permit = car.getPermit();  
             totalCharges = totalCharges.add(getParkingCharges(permit));  
         }
-
         return totalCharges;
     }
 
