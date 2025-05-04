16a1dfba6fd445b949f1a08bdb080a1d836c3cf3
PeterFedor-DU <peter.fedor@du.edu>
2025-05-04 16:29:19 -0600

    Update TransactionManager.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/TransactionManager.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/TransactionManager.java
@@ -15,12 +15,28 @@ public class TransactionManager {
 
     public ParkingTransaction park(Calendar date, ParkingPermit permit, ParkingLot lot) {
         LocalDateTime entryTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        LocalDateTime exitTime = entryTime.plusHours(1);
+        LocalDateTime exitTime = entryTime.plusHours(1); 
 
         ParkingChargeStrategy strategy = ParkingChargeStrategyFactory.getStrategy(lot);
         Money feeCharged = strategy.calculateCharge(permit, entryTime, exitTime, lot.getBaseRate());
-///Creating the transaction and storing it
-        ParkingTransaction transaction = new ParkingTransaction(date, permitID, lot, feeCharged);
+
+        ParkingTransaction transaction = new ParkingTransaction(permit, entryTime, exitTime, feeCharged);
+        transactions.add(transaction);
+        return transaction;
+    }
+
+///New method for oberserver 
+    public ParkingTransaction park(ParkingEvent event) {
+        ParkingPermit permit = event.getPermit();
+        ParkingLot lot = event.getLot();
+
+        LocalDateTime exitTime = event.getTimestamp(); 
+        LocalDateTime entryTime = exitTime.minusHours(1); 
+
+        ParkingChargeStrategy strategy = ParkingChargeStrategyFactory.getStrategy(lot);
+        Money feeCharged = strategy.calculateCharge(permit, entryTime, exitTime, lot.getBaseRate());
+
+        ParkingTransaction transaction = new ParkingTransaction(permit, entryTime, exitTime, feeCharged);
         transactions.add(transaction);
         return transaction;
     }
@@ -36,16 +52,16 @@ public class TransactionManager {
     }
 
     public Money getParkingCharges(Customer customer) {
-        Money totalCharges = new Money(0);  
+        Money totalCharges = new Money(0);
         for (Car car : customer.getCars()) {
-            String permit = car.getPermit();  
-            totalCharges = totalCharges.add(getParkingCharges(permit));  
+            ParkingPermit permit = car.getPermit(); 
+            totalCharges = totalCharges.add(getParkingCharges(permit));
         }
         return totalCharges;
     }
 
     public Money getParkingCharges(String licensePlate) {
-        Money totalCharges = new Money(0); 
+        Money totalCharges = new Money(0);
         for (ParkingTransaction transaction : transactions) {
             if (transaction.getPermit().getCar().getLicense().equals(licensePlate)) {
                 totalCharges = totalCharges.add(transaction.getFeeCharged());
