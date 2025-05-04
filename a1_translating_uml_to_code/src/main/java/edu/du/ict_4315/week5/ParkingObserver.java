5637554b3801421f774321309c692606dad1fca8
PeterFedor-DU <peter.fedor@du.edu>
2025-05-04 16:31:09 -0600

    Update ParkingObserver.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/week5/ParkingObserver.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/week5/ParkingObserver.java
@@ -1,1 +1,16 @@
+package edu.du.ict_4315;
 
+public class ParkingObserver implements ParkingAction {
+    private final TransactionManager manager;
+
+    public ParkingObserver(TransactionManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void update(ParkingEvent event) {
+        if (!event.isEntry()) { 
+            manager.park(event);
+        }
+    }
+}
