01a39b760fee53f8018c6b022716e4bff9719965
PeterFedor-DU <peter.fedor@du.edu>
2025-05-04 16:26:22 -0600

    Update ParkingEvent.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/week5/ParkingEvent.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/week5/ParkingEvent.java
@@ -1,1 +1,22 @@
+package edu.du.ict_4315;
 
+import java.time.LocalDateTime;
+
+public class ParkingEvent {
+    private final ParkingPermit permit;
+    private final ParkingLot lot;
+    private final LocalDateTime timestamp;
+    private final boolean isEntry;
+
+    public ParkingEvent(ParkingPermit permit, ParkingLot lot, boolean isEntry) {
+        this.permit = permit;
+        this.lot = lot;
+        this.timestamp = LocalDateTime.now();
+        this.isEntry = isEntry;
+    }
+
+    public ParkingPermit getPermit() { return permit; }
+    public ParkingLot getLot() { return lot; }
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public boolean isEntry() { return isEntry; }
+}
