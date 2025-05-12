fc4a840f3ba2c4fd6ffd389da4131c0e4077a9f6
PeterFedor-DU <peter.fedor@du.edu>
2025-05-11 23:10:25 -0600

    Create FlatRateCalculator.java

---
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/decorator/FlatRateCalculator.java
@@ -0,0 +1,17 @@
+package edu.du.ict4315.charges.decorator;
+
+import java.time.LocalDateTime;
+import edu.du.ict4315.parking.ParkingLot;
+import edu.du.ict4315.parking.ParkingPermit;
+import edu.du.ict4315.money.Money;
+
+public class FlatRateCalculator extends ParkingChargeCalculator {
+
+    private static final Money FLAT_RATE = new Money(10.00);
+
+    @Override
+    public Money getParkingCharge(LocalDateTime entryTime, LocalDateTime exitTime,
+                                  ParkingLot lot, ParkingPermit permit) {
+        return FLAT_RATE;
+    }
+}
