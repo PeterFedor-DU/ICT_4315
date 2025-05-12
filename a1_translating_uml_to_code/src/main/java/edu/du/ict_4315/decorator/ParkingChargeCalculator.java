fada2006b34dc0b7f2969113d36fdbac313c1f77
PeterFedor-DU <peter.fedor@du.edu>
2025-05-11 23:04:33 -0600

    Update and rename ParkingChargeCalculatorDecorator to ParkingChargeCalculator.java

---
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/decorator/ParkingChargeCalculator.java
@@ -0,0 +1,11 @@
+package edu.du.ict4315.decorator;
+
+import java.time.LocalDateTime;
+import edu.du.ict4315.parking.ParkingLot;
+import edu.du.ict4315.parking.ParkingPermit;
+import edu.du.ict4315.money.Money;
+
+public abstract class ParkingChargeCalculator {
+    public abstract Money getParkingCharge(LocalDateTime entryTime, LocalDateTime exitTime,
+                                           ParkingLot lot, ParkingPermit permit);
+}
