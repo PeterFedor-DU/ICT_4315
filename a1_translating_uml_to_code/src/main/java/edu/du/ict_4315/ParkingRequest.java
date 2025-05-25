a76080f8703627c0d3eb8f9434f98daac397dec6
PeterFedor-DU <peter.fedor@du.edu>
2025-05-25 16:33:03 -0600

    Update ParkingRequest.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingRequest.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingRequest.java
@@ -1,26 +1,55 @@
-package edu.du.ict_4315
+package edu.du.ict_4315;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Properties;
 
 public class ParkingRequest {
-    private String command;
+    private String commandName;
     private Properties parameters;
 
-    public ParkingRequest(String command, Properties parameters) {
-        this.command = command;
+    // Default constructor required for Gson
+    public ParkingRequest() {
+        this.parameters = new Properties();
+    }
+
+    public ParkingRequest(String commandName, Properties parameters) {
+        this.commandName = commandName;
         this.parameters = parameters;
     }
 
-    public String getCommand() {
-        return command;
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public void setCommandName(String commandName) {
+        this.commandName = commandName;
     }
 
     public Properties getParameters() {
         return parameters;
     }
 
+    public void setParameters(Properties parameters) {
+        this.parameters = parameters;
+    }
+
+    public String toJson() {
+        Gson gson = new GsonBuilder().create();
+        return gson.toJson(this);
+    }
+
+    public static ParkingRequest fromJson(String json) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(json, ParkingRequest.class);
+    }
+
     @Override
     public String toString() {
-        return "ParkingRequest [command=" + command + ", parameters=" + parameters + "]";
+        return "ParkingRequest{" +
+                "commandName='" + commandName + '\'' +
+                ", parameters=" + parameters +
+                '}';
     }
 }
