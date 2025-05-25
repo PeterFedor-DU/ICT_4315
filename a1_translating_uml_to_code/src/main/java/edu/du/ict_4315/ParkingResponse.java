ea063273de3930e29b134a5b94f03f2c001d3cc1
PeterFedor-DU <peter.fedor@du.edu>
2025-05-25 16:34:56 -0600

    Update ParkingResponse.java

--- a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingResponse.java
+++ a1_translating_uml_to_code/src/main/java/edu/du/ict_4315/ParkingResponse.java
@@ -1,5 +1,8 @@
 package edu.du.ict_4315
 
+
+import com.google.gson.Gson;
+
 public class ParkingResponse {
     private int statusCode;
     private String message;
@@ -19,6 +22,18 @@ public class ParkingResponse {
 
     @Override
     public String toString() {
-        return "ParkingResponse [statusCode=" + statusCode + ", message=" + message + "]";
+        return "ParkingResponse{statusCode=" + statusCode + ", message='" + message + "'}";
+    }
+
+    // Convert this object to JSON string
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    // Convert JSON string to ParkingResponse object
+    public static ParkingResponse fromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, ParkingResponse.class);
     }
 }
